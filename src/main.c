8b2370ac2758ba343dd5412fe74867a4ad4f3aba
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-01 23:15:57 +0100

    display offset

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 16
+
+void putstr(const char* s, int fd) {
+    write(fd, s, strlen(s));
+}
 
 bool is_printable(char c) {
     // below are control characters, above (0x7f) is DEL
@@ -10,7 +15,7 @@ bool is_printable(char c) {
 }
 
 // displays non printable characters as dots
-void display(const char* input) {
+void display_ascii_with_dots(const char* input, int fd) {
     char line[BUFFER_SIZE + 1] = {0};
     strncpy(line, input, BUFFER_SIZE);
 
@@ -20,17 +25,24 @@ void display(const char* input) {
         }
     }
 
-    write(STDOUT_FILENO, line, strlen(line));
-    write(STDOUT_FILENO, "\n", 1);
+    putstr(line, fd);
 }
+
+// sprintf maybe
+void display_hex(size_t offset, size_t n_digits);
 
 int main(void) {
     char line[BUFFER_SIZE + 1] = {0};
     size_t bytes_read;
+    size_t offset = 0;
 
     bytes_read = read(STDIN_FILENO, line, BUFFER_SIZE);
     while (bytes_read > 0) {
-        display(line);
+        printf("%04zx: ", offset);
+        fflush(stdout);
+        offset += bytes_read;
+        display_ascii_with_dots(line, STDOUT_FILENO);
+        putstr("\n", STDOUT_FILENO);
         memset(line, 0, BUFFER_SIZE + 1);
         bytes_read = read(STDIN_FILENO, line, BUFFER_SIZE);
     }
