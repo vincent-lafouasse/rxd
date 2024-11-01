1aee64df25c8b832708f5e42a4d2832916c568e8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-01 22:44:11 +0100

    simplify

--- src/main.c
+++ src/main.c
@@ -1,19 +1,17 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 16
 
 int main(void) {
-	unsigned char line[BUFFER_SIZE];
+	unsigned char line[BUFFER_SIZE + 1] = {0};
 	size_t bytes_read;
 
 	bytes_read = read(STDIN_FILENO, line, BUFFER_SIZE);
-	while (bytes_read == BUFFER_SIZE) {
-		write(STDOUT_FILENO, line, BUFFER_SIZE);
-		write(STDOUT_FILENO, "\n", 1);
-		memset(line, 0, BUFFER_SIZE);
+	while (bytes_read > 0) {
+		printf("%s\n", line);
+		memset(line, 0, BUFFER_SIZE + 1);
 		bytes_read = read(STDIN_FILENO, line, BUFFER_SIZE);
 	}
-	write(STDOUT_FILENO, line, bytes_read);
-	write(STDOUT_FILENO, "\n", 1);
 }
