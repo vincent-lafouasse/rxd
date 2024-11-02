1ee091c65ccad739565e6fe1027b312989dc1f66
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-02 18:08:04 +0100

    simplify using fprintfs buffering instead of doing our own

--- src/main.c
+++ src/main.c
@@ -4,10 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-
-#define LOWERCASE_HEX "0123456789abcdef"
-#define UPPERCASE_HEX "0123456789abcdef"
 
 typedef uint8_t u8;
 typedef uint32_t u32;
@@ -26,53 +22,22 @@ bool is_printable(char c);
 void display_bytes_hex(const char* input, const DisplayConfig cfg) {
     assert(cfg.line_width != 0);
 
-    char* buffer = malloc(3 * cfg.line_width +
-                          1);  // each bytes needs 2 hex digits and a space
-    buffer[3 * cfg.line_width] = '\0';
-    for (size_t i = 0; i < cfg.line_width; i++) {
-        u8 byte = input[i];
-        buffer[3 * i] = LOWERCASE_HEX[byte / 16];
-        buffer[3 * i + 1] = LOWERCASE_HEX[byte % 16];
-        buffer[3 * i + 2] = ' ';
+    for (size_t i = 0; input[i] && i < cfg.line_width; i++) {
+        fprintf(cfg.out, "%02x", (u32)input[i]);
+        if (i % 2 == 1) {
+            fprintf(cfg.out, " ");
+        }
     }
-    fprintf(cfg.out, "%s", buffer);
-    free(buffer);
 }
 
 // displays non printable characters as dots
-void display_bytes_ascii(const char* input, const DisplayConfig cfg) {
+void display_bytes_ascii(const char* line, const DisplayConfig cfg) {
     assert(cfg.line_width != 0);
-    char* line = calloc(cfg.line_width + 1, 1);
-    strncpy(line, input, cfg.line_width);
 
-    for (size_t i = 0; line[i]; i++) {
-        if (!is_printable(line[i])) {
-            line[i] = '.';
-        }
+    for (size_t i = 0; line[i] && i < cfg.line_width; i++) {
+        char to_print = is_printable(line[i]) ? line[i] : '.';
+        printf("%c", to_print);
     }
-
-    fprintf(cfg.out, "%s", line);
-    free(line);
-}
-
-void display_u32_hex(u32 n, const DisplayConfig cfg) {
-    size_t width = 2 * sizeof(n);  // 2 hex per byte
-
-    char* buffer = malloc(width + 1);
-    memset(buffer, '0', width);
-    buffer[width] = '\0';
-
-    for (size_t i = 0; i < width; i++) {
-        size_t digit = n % 16;
-        if (digit < 10)
-            buffer[i] = '0' + digit;
-        else
-            buffer[i] = (digit - 10) + 'A';
-        n = n / 16;
-    }
-    mem_reverse(buffer, width);
-    fprintf(cfg.out, "%s", buffer);
-    free(buffer);
 }
 
 int main(void) {
@@ -83,8 +48,7 @@ int main(void) {
 
     bytes_read = fread(line, cfg.line_width, 1, cfg.in);
     while (bytes_read > 0) {
-        display_u32_hex(offset, cfg);
-        fprintf(cfg.out, ": ");
+        fprintf(cfg.out, "%08u: ", offset);
         offset += bytes_read;
         display_bytes_hex(line, cfg);
         fprintf(cfg.out, " ");
@@ -103,14 +67,4 @@ DisplayConfig default_config(void) {
 bool is_printable(char c) {
     // below are control characters, above (0x7f) is DEL
     return (c >= 0x20 && c <= 0x7e);
-}
-
-void mem_reverse(void* __bytes, size_t n) {
-    u8* bytes = __bytes;
-
-    for (size_t i = 0; i < n / 2; i++) {
-        u8 tmp = bytes[i];
-        bytes[i] = bytes[n - 1 - i];
-        bytes[n - 1 - i] = tmp;
-    }
 }
